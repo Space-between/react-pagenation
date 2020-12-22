c71b2663258cac3bcad09e5fb1812455d3f7bf3f
Kim Tae Yong <57104644+Space-between@users.noreply.github.com>
2020-12-22 17:05:14 +0900

    카카오 API를 이용한 다음 검색창따라하기

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "antd";
+import axios from "axios";
 
 const { Search } = Input;
-const onSearch = (value: any) => console.log(value);
 
 export default () => {
+  const [info, setInfo] = useState<Array<object> | null>(null);
+  // console.log("info", info);
+  const onSearch = async (value: string) => {
+    try {
+      await axios({
+        method: "get",
+        url: `https://dapi.kakao.com/v2/search/web?query=${value}&size=50`,
+        headers: { Authorization: "KakaoAK 6c0bb4f5679728a4f6ddcef0e5eb6bd8" },
+      }).then(function (response) {
+        console.log(response);
+        const abc = response.data.documents;
+        setInfo(abc);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <Search placeholder="input search text" onSearch={onSearch} enterButton />
+      <Search
+        placeholder="카카오 API를 이용한 다음 검색창"
+        onSearch={onSearch}
+        enterButton
+      />
+      {console.log("info", typeof info)}
+      <ul>
+        {info
+          ? info.map((data: any) => (
+              <a href={data.url} style={{ color: "black" }}>
+                <li>{data.contents}</li>
+              </a>
+            ))
+          : null}
+      </ul>
     </div>
   );
 };
